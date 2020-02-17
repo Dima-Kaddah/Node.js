d31122d4ea09e020b4f1cdf03797e291dc9bd694
Dima <dimakaddah@gmail.com>
2020-02-17 02:43:01 +0100

    add exercise2 npm package

---
+++ week1/homework/nodejs-exercises/exercise2/app.js
@@ -0,0 +1,16 @@
+//call package
+const leftPad = require('left-pad');
+
+let numbers = ['12', '846', '2', '1236'];
+
+//set function can take any array
+function pad(arr) {
+  arr.forEach(num => {
+    //try it with 4 using the package "leftPad"
+    console.log(leftPad(num, 4, '_'));
+    //try it with 8 using the package "leftPad"
+    console.log(leftPad(num, 8, '_'));
+  });
+}
+//call function
+pad(numbers);
