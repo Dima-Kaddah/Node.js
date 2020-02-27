bb0f6f172a7f7df52de517933d4fff0cc442bc53
Dima <57494966+Dima-Kaddah@users.noreply.github.com>
2020-02-28 00:51:01 +0100

    Delete app.js

--- week1/homework/nodejs-exercises/exercise1/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const padLeft = require('./andrejs-awesome-function');
-const numbers = ['12', '846', '2', '1236'];
-numbers.forEach(number => {
-  console.log(padLeft(number, 5, '_') + ';');
-});
