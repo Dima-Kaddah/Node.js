7bc9d6fe65b83c24b38ed35411bcfe2bef6553ca
Dima <57494966+Dima-Kaddah@users.noreply.github.com>
2020-02-28 00:52:42 +0100

    Delete server.js

--- week1/homework/nodejs-exercises/HackYourTemperature I/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const expressHandlebars = require('express-handlebars');
-const axios = require('axios');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('hello from backend to frontend!'));
-app.listen(port, () => console.log(`server at ${port}`));
