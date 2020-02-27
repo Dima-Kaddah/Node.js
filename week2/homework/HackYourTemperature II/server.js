43d136869bc53a78e66e227d4f4fe65d3295f7b4
Dima <dimakaddah@gmail.com>
2020-02-27 21:56:29 +0100

    add week2 part2 project  HackYourTemperature II

--- week2/homework/HackYourTemperature II/server.js
+++ week2/homework/HackYourTemperature II/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const expressHandlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = process.env.port || 4000;
 
-app.get('/', (req, res) => res.send('hello from backend to frontend!'));
+//for data type
+app.use(express.urlencoded({ extended: true }));
+//making templates to create a frontend that will be a simple page with a form
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+// The Backend//creating a POST route that will allow us to access the submitted form data.
+app.post('/weather', (req, res) => {
+  const newCityName = req.body.cityName;
+  res.render('index', { cityName: newCityName });
+});
 app.listen(port, () => console.log(`server at ${port}`));
