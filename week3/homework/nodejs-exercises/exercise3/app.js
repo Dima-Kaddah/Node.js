1dbb19d1120df2f9dfde403580b46d86cab4b5a7
Dima <dimakaddah@gmail.com>
2020-03-06 22:00:36 +0100

    add exercise3

---
+++ week3/homework/nodejs-exercises/exercise3/app.js
@@ -0,0 +1,28 @@
+'use strict';
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+const port = 3000;
+const URL_API = 'https://reservation100-sandbox.mxapps.io/api/reservations';
+
+const dataBody = {
+  "name": "Dima Kaddah",
+  "numberOfPeople": 13,
+}
+
+app.get('/', (req, res) => {
+  fetch(URL_API, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(dataBody),
+  })
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.error(`somthing went wrong ${err}`);
+    });
+});
+app.listen(port, () => console.log(`Listening to Port ${port}`));
+
+//curl -X POST "https://reservation100-sandbox.mxapps.io/api/reservations" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"name\": \"Dima Kaddah\", \"numberOfPeople\": 13}`"
